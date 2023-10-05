850a6c9e49bc7f3681710809aaabce3cf68cf155
snambiaroff <144677752+snambiaroff@users.noreply.github.com>
2023-10-05 12:12:12 +0530

    Create pgm6.py

---
+++ pgm6.py
@@ -0,0 +1,15 @@
+def add_suffix(string):
+
+    if len(string) < 3:
+        return string
+    elif string[-3:] == "ing":
+        return string + "ly"
+    else:
+        return string + "ing"
+
+
+string = "abc"
+result = add_suffix(string)
+print(result)
+
+string = "string"
