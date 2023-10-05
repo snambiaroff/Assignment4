c1922ed3eabb93895758ecfdbc6950e442cf355f
snambiaroff <144677752+snambiaroff@users.noreply.github.com>
2023-10-05 12:12:41 +0530

    Create pgm7.py

---
+++ pgm7.py
@@ -0,0 +1,15 @@
+import re
+
+
+def replace_not_poor(string):
+
+    pattern = r"(?<=poor)\snot"
+    result = re.sub(pattern, " good", string)
+    return result
+
+
+string = "The lyrics is not that poor!"
+result = replace_not_poor(string)
+print(result)
+
+string = "The lyrics is poor!"
