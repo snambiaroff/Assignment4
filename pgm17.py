d98b48c977110ebcc13ce1d21de6639a6ed6931d
snambiaroff <144677752+snambiaroff@users.noreply.github.com>
2023-10-05 12:21:15 +0530

    Create pgm17.py

---
+++ pgm17.py
@@ -0,0 +1,14 @@
+def convert_string_to_list(string):
+
+  list_of_characters = list(string)
+  return list_of_characters
+
+
+
+
+string = "Hello, world!"
+list_of_characters = convert_string_to_list(string)
+print(list_of_characters)
+
+
+
