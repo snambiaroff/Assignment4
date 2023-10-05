a89adc882d3025e18879c6afb202366f002e7857
snambiaroff <144677752+snambiaroff@users.noreply.github.com>
2023-10-05 12:18:31 +0530

    Create pgm14.py

---
+++ pgm14.py
@@ -0,0 +1,10 @@
+floating_number = 3.1415926
+
+# Round the floating number to 2 decimal places.
+rounded_floating_number = round(floating_number, 2)
+
+# Print the rounded floating number.
+print(rounded_floating_number)
+
+
+
