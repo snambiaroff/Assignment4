e8cab912627368811d2c04657239076ebfa89526
snambiaroff <144677752+snambiaroff@users.noreply.github.com>
2023-10-05 12:10:33 +0530

    Create pgm3.py

---
+++ pgm3.py
@@ -0,0 +1,15 @@
+def string_both_ends(string):
+
+    if len(string) < 2:
+        return ""
+    else:
+        return string[:2] + string[-2:]
+
+
+string = "thisisniceone"
+result = string_both_ends(string)
+print(result)
+
+string = "ab"
+result = string_both_ends(string)
+print(result)
